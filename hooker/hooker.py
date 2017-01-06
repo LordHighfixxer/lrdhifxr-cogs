a83fb11c106afbd63f975ea5d3cfc8433b04c84e
LordHighfixxer <lordhighfixxer@protonmail.com>
2017-01-06 12:41:39 -0500

    Add files via upload

---
+++ hooker/hooker.py
@@ -0,0 +1,14 @@
+class Hooker:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def listener(self, message):
+        if message.author.id != self.bot.user.id:
+            if 'blowjob' in message.content.lower() or 'hummer' in message.content.lower() or 'blowie' in message.content.lower():
+                await self.bot.send_message(message.channel, 'Did someone mention a blowjob because, I mean lets talk prices.')
+
+
+def setup(bot):
+    n = Hooker(bot)
+    bot.add_listener(n.listener, "on_message")
+    bot.add_cog(n)
