d3181ee2ba44083d58d9d848a485bf6b339ad457
LordHighfixxer <lordhighfixxer@protonmail.com>
2017-03-21 10:50:53 -0400

    Update cybill.py
    Initial testing commit

--- cybil/cybill.py
+++ cybil/cybill.py
@@ -1,13 +1,26 @@
+import discord
+from discord.ext import commands
+from random import choice as randchoice
+
+class Cybilshit:
+    """Display Cybil statements"""
+
+    def __init__(self, bot):
+        self.bot = bot
+self.cybilshit = ["I could kick your ass at this game Adam","Wow, you suck Adam.","Can you not see I am getting ready for work Adam?","What does don't stick your dick in crazy mean Adam?"]
+
 class Cybil:
     def __init__(self, bot):
         self.bot = bot
 
     async def listener(self, message):
-        if message.author.id != self.bot.user.id:
-            if 'blowjob' in message.content.lower() or 'hummer' in message.content.lower() or 'blowie' in message.content.lower():
-                await self.bot.send_message(message.channel, 'Did someone mention a blowjob because, I mean lets talk prices.')
-
-
+       auth = member.id == "121246220382502912"
+       if 'cybil' in message.content.lower():
+            fuck = randchoice(self.cybilshit).format(auth.mention)
+            data = discord.Embed(colour=user.colour)
+            data.add_field(name="Fuck You!",value="{}".format(fuck))
+            await self.bot.say(embed=data)
+            
 def setup(bot):
     n = Cybil(bot)
     bot.add_listener(n.listener, "on_message")
