bf6757889dc35666e6a310cad2562b4dde5137b2
LordHighfixxer <lordhighfixxer@protonmail.com>
2017-04-19 11:24:48 -0400

    Update GNN.py
    Back to Vanilla

--- GNN/GNN.py
+++ GNN/GNN.py
@@ -52,8 +52,7 @@ class Newsletter:
                 await self.bot.say("Congrats, you will now stop recieving newsletter thru pm!")
         else:
             await self.bot.say("{}, uou need a newsletter accoount to start receiving the latest info. Say `{}newsletter signup` now!".format(weeb.mention, ctx.prefix))
-
-    @checks.ctx.messsage.author.id in ["164416062337712128", "97375528868118528"]
+   
     @newsletter.command(pass_context=True)
     async def send(self, ctx, *, msg):
         """Allows Authorized Users Only to Send out Newsletter!"""
