8a40ef7f7b6ab8a0bcb45d56129c93c2ef6c1524
LordHighfixxer <lordhighfixxer@protonmail.com>
2017-04-19 12:02:52 -0400

    Update GNN.py
    Perms: Oz - Adam - Spark

--- GNN/GNN.py
+++ GNN/GNN.py
@@ -57,7 +57,7 @@ class Newsletter:
     async def send(self, ctx, *, msg):
         """Allows Authorized Users Only to Send out Newsletter!"""
         
-        if ctx.message.author.id not in ["164416062337712128", "97375528868118528", "121246220382502912"]:
+        if ctx.message.author.id not in ["121246220382502912", "149652374791651339", "97375528868118528"]:
             await self.bot.say("You don't have permission to send out newsletters.")
             return
                     
