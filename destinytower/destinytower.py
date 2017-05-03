3b039b2c2d23ab5e93c6d3d72d313de5532498b8
LordHighfixxer <lordhighfixxer@protonmail.com>
2017-05-03 11:26:31 -0400

    Update destinytower.py
    PyCharm edited

--- destinytower/destinytower.py
+++ destinytower/destinytower.py
@@ -30,10 +30,10 @@ class DestinyTower:
                "Will Fireteam.... (sigh) Will Fireteam 'The Bad Guys don't care what we call ourselves' please report to the Vanguard immediately.",
                "Shuttle Advisory: The Crucible is open to those who run it.",
                "Shuttle to Core East in 6 minutes!",
-	       "Shuttle to Lower North drops off, 6 minutes!",
-	       "This is Master Rahool. Apprentice DeSalnus, please return to the archives at once, your engrams are melting a table!",
-	       "Fireteam Swift Resolution, you have been reassigned. Report to the wall. Zavala out.",
-	       "Fire team The Vanguard Don't Care What We Call Ourselves Either, report in - Zavalla",
+	           "Shuttle to Lower North drops off, 6 minutes!",
+	           "This is Master Rahool. Apprentice DeSalnus, please return to the archives at once, your engrams are melting a table!",
+	           "Fireteam Swift Resolution, you have been reassigned. Report to the wall. Zavala out.",
+	           "Fire team The Vanguard Don't Care What We Call Ourselves Either, report in - Zavalla",
                "Attention we have reports of a fireteam leader involuntarily extracting Guardians to orbit. Please report these incidents to the Vanguard immediately."]
 
         destinytower = choice(destinytowerquote)
