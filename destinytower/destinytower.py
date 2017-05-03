e1fb3d6ad66ee7bdb1104732432cf549a2b397ea
LordHighfixxer <lordhighfixxer@protonmail.com>
2017-05-03 11:36:19 -0400

    Update destinytower.py
    Trying this again

--- destinytower/destinytower.py
+++ destinytower/destinytower.py
@@ -3,36 +3,34 @@ from discord.ext import commands
 from random import choice
 
 
-class DestinyTower:
+class destinytower:
     "Delivers a Random Destiny Tower PA Quote"
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(no_pm=True, aliases=["destinytower"])
-    async def towerpa(self):
+    async def destinytower(self):
         """Displays a random Quote from Destinys Tower PA."""
 
         # TODO make bigger list
-        destinytowerquote = ["Attention Guardians, attacks on Titans have increased. Please stay in a fireteam to remain safe.",
-               "Guardians, having a Ghost is no excuse for recreational Tower jumping!",
-               "Dead Orbit would like to remind everyone that their pessimistic name does not prevent an optimistic outcome....if we survive.",
-               "Hello, this is Cayde-6 of the Vanguard. I realize this is an abuse of the Tower P.A., but whoever took my sparrow, I will find you. And you'll wear a sign that says you stole... Nevermind, nevermind! It's right here.",
-               "Fireteam... Fireteam 'The Bad Guys Don't Care What We Call Ourselves Do They?' report in. Zavala out.",
-               "Robots are not target practice, Guardians.",
-               "Warning: unauthorized use of a Fist of Havoc reported in the shuttle bay",
-               "This is Ikora Rey. The South Tower is off limits, because we are still cleaning up last night's... event.",                  
-               "You are a great Guardian, and your mom is a classy lady.",                  
-               "All Titans interested in Commander Zavala’s crochet course should speak with a Frame for details.", 
-               "Guardian Alert: Fallen are the enemy.",                   
-               "Flight Controller 5530, signing off. I quit.",                  
-               "Fireteam Bad News for the Bad Guys report in immediately, Zavala out.",                  
-               "Will Fireteam.... (sigh) Will Fireteam 'The Bad Guys don't care what we call ourselves' please report to the Vanguard immediately.",
-               "Shuttle Advisory: The Crucible is open to those who run it.",
-               "Shuttle to Core East in 6 minutes!",	         
-               "Attention we have reports of a fireteam leader involuntarily extracting Guardians to orbit. Please report these incidents to the Vanguard immediately."]
-
-        destinytower = choice(destinytowerquote)
+        destinytower = ["http://i.imgur.com/HpXZPZ1.gif",
+               "http://i.imgur.com/KsCh67n.gif",
+               "http://i.imgur.com/HIZfEpX.gif",
+               "http://i.imgur.com/uzubEP8.gif",
+               "http://i.imgur.com/SRQYQjy.jpg",
+               "http://i.imgur.com/clPnnul.gif",
+               "http://i.imgur.com/k6JwlGp.jpg",
+               "http://i.imgur.com/3ihp9Gl.gif",                  
+               "http://i.imgur.com/EAWFfAZ.jpg",                  
+               "http://i.imgur.com/uBywNQO.jpg", 
+               "http://i.imgur.com/voBmkuO.gif",                   
+               "http://i.imgur.com/r1gl4Nl.jpg",                  
+               "http://i.imgur.com/BKrcNTs.jpg",                  
+               "http://i.imgur.com/GskTVj0.gif",
+               "http://i.imgur.com/pKQCbDO.gif",                  
+               "http://i.imgur.com/yXgXd4W.gif"]
+        destinytower = choice(destinytower)
 
         destinytowersay = ["destinytower!!11", "destinytower Intensification Protocol Engaged", "Keeeeeeesssssssel",
                   "destinytower destinytower :P", "*intense destinytowering*", "I ran out of destinytower so here is some more"]
@@ -54,4 +52,4 @@ class DestinyTower:
 
 
 def setup(bot):
-     bot.add_cog(DestinyTower(bot))
+     bot.add_cog(destinytower(bot))
