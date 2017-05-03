6d4eda121bb948a2e2f347db4dd1bed418daf119
LordHighfixxer <lordhighfixxer@protonmail.com>
2017-05-03 11:55:42 -0400

    Update destinytower.py
    big change

--- destinytower/destinytower.py
+++ destinytower/destinytower.py
@@ -3,47 +3,52 @@ from discord.ext import commands
 from random import choice
 
 
-class phlem:
-    "phlem phlem"
+class quotepa:
+    "quotepa quotepa"
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(no_pm=True, aliases=["phlem"])
-    async def phlem(self):
-        """Displays a random phlem."""
+    @commands.command(no_pm=True, aliases=["quotepa"])
+    async def quotepa(self):
+        """Displays a random quotepa."""
 
         # TODO make bigger list
-        phlem = ["http://i.imgur.com/HpXZPZ1.gif",
-               "http://i.imgur.com/KsCh67n.gif",
-               "http://i.imgur.com/HIZfEpX.gif",
-               "http://i.imgur.com/uzubEP8.gif",
-               "http://i.imgur.com/SRQYQjy.jpg",
-               "http://i.imgur.com/clPnnul.gif",
-               "http://i.imgur.com/k6JwlGp.jpg",
-               "http://i.imgur.com/3ihp9Gl.gif",                  
-               "http://i.imgur.com/EAWFfAZ.jpg",                  
-               "http://i.imgur.com/uBywNQO.jpg", 
-               "http://i.imgur.com/voBmkuO.gif",                   
-               "http://i.imgur.com/r1gl4Nl.jpg",                  
-               "http://i.imgur.com/BKrcNTs.jpg",                  
-               "http://i.imgur.com/GskTVj0.gif",
-               "http://i.imgur.com/pKQCbDO.gif",                  
-               "http://i.imgur.com/yXgXd4W.gif"]
+        quotepa = ["Attention Guardians, attacks on Titans have increased. Please stay in a fireteam to remain safe.",
+               "Guardians, having a Ghost is no excuse for recreational Tower jumping!",
+               "Dead Orbit would like to remind everyone that their pessimistic name does not prevent an optimistic outcome....if we survive.",
+               "Hello, this is Cayde-6 of the Vanguard. I realize this is an abuse of the Tower P.A., but whoever took my sparrow, I will find you. And you'll wear a sign that says you stole... Nevermind, nevermind! It's right here.",
+               "Fireteam... Fireteam 'The Bad Guys Don't Care What We Call Ourselves Do They?' report in. Zavala out.",
+               "Robots are not target practice, Guardians.",
+               "Warning: unauthorized use of a Fist of Havoc reported in the shuttle bay",
+               "This is Ikora Rey. The South Tower is off limits, because we are still cleaning up last night's... event.",                  
+               "You are a great Guardian, and your mom is a classy lady.",                  
+               "All Titans interested in Commander Zavala’s crochet course should speak with a Frame for details.", 
+               "Guardian Alert: Fallen are the enemy.",                   
+               "Flight Controller 5530, signing off. I quit.",                  
+               "Fireteam Bad News for the Bad Guys report in immediately, Zavala out.",                  
+               "Will Fireteam.... (sigh) Will Fireteam 'The Bad Guys don't care what we call ourselves' please report to the Vanguard immediately.",
+               "Shuttle Advisory: The Crucible is open to those who run it.",!kesse
+               "Shuttle to Core East in 6 minutes!",
+			   "Shuttle to Lower North drops off, 6 minutes!",
+			   "This is Master Rahool. Apprentice DeSalnus, please return to the archives at once, your engrams are melting a table!",
+			   "Fireteam Swift Resolution, you have been reassigned. Report to the wall. Zavala out.",
+			   "Fire team The Vanguard Don't Care What We Call Ourselves Either, report in - Zavalla",
+               "Attention we have reports of a fireteam leader involuntarily extracting Guardians to orbit. Please report these incidents to the Vanguard immediately."]
 
-        phlem = choice(phlem)
+        quotepa = choice(quotepa)
 
-        phlemsay = ["phlem!!11", "phlem Intensification Protocol Engaged", "Keeeeeeesssssssel",
-                  "phlem phlem :P", "*intense phleming*", "I ran out of phlem so here is some more"]
-        phlemsay = choice(phlemsay)
+        quotepasay = ["quotepa!!11", "quotepa Intensification Protocol Engaged", "Keeeeeeesssssssel",
+                  "quotepa quotepa :P", "*intense quotepaing*", "I ran out of quotepa so here is some more"]
+        quotepasay = choice(quotepasay)
 
         colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         colour = int(colour, 16)
 
         data = discord.Embed(
             description="", colour=discord.Colour(value=colour))
-        data.add_field(name=phlemsay, value=u"\u2063")
-        data.set_image(url=phlem)
+        data.add_field(name=quotepasay, value=u"\u2063")
+        data.set_image(url=quotepa)
 
         try:
             await self.bot.say(embed=data)
@@ -53,4 +58,4 @@ class phlem:
 
 
 def setup(bot):
-     bot.add_cog(phlem(bot))
+     bot.add_cog(quotepa(bot))
