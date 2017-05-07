9b52a5056674e48c362a68c18f31b2b299839d39
LordHighfixxer <lordhighfixxer@protonmail.com>
2017-05-07 09:06:02 -0400

    Update destinymotes.py
    Revert and 15 syntax fix

--- destinymotes/destinymotes.py
+++ destinymotes/destinymotes.py
@@ -2,19 +2,20 @@ from discord.ext import commands
 
 
 class Detinymotes:
-	def __init__(self, bot):
-        	self.bot = bot
-        	self.base = 'data/memes/images/'
-	@commands.group(pass_context=True, no_pm=True, name='dmote')
-    	async def _dmote(self, context):
-        	"""Calls Destiny Emotes"""
-        	if context.invoked_subcommand is None:
-            		await send_cmd_help(context)
+    def __init__(self, bot):
+        self.bot = bot
+        self.base = 'data/memes/images/'
+	
+    @commands.group(pass_context=True, no_pm=True, name='dmote')
+    async def _dmote(self, context):
+        """Calls Destiny Emotes"""
+        if context.invoked_subcommand is None:
+            await send_cmd_help(context)
 	
         @_dmote.command(pass_context=True, aliases=;['zpalm'])
-        async def _zavalafaceplam(self, context):
-		await self.bot.send_file(context.message.channel, '{}zavala_facepalm.png'.format(self.base))
-	
+        async def _zavala_faceplam(self, context):
+            await self.bot.send_file(context.message.channel, '{}zavala_facepalm.png'.format(self.base))
+    
 def setup(bot):
     n = Destinymotes(bot)
-    bot.add_cog(n)
+bot.add_cog(n)
